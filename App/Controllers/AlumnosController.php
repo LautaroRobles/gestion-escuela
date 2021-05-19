0b9c2d6cb28cea40078247308e7c5d3d719901ac
LautaroRobles <lautarorobles_oro@hotmail.com>
2021-05-19 20:08:43 -0300

    Prueba 1 con composer

---
+++ App/Controllers/AlumnosController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controllers;
+
+class AlumnosController {
+    var $edible;
+    var $color;
+
+    function __construct()
+    {
+    }
+
+    public static function getAllAlumnos() {
+        echo 'op';
+        return "asd";
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// Require composer autoloader
+require __DIR__ . '/vendor/autoload.php';
+
+// Create Router instance
+$router = new \Bramus\Router\Router();
+
+// Define routes
+$router->setNamespace('\App\Controllers');
+$router->get('/about', 'AlumnosController@getAllAlumnos');
+$router->get('/', function() {
+    echo 'About Page Contents';
+});
+
+// Run it!
+$router->run();
+
+?>
